cdb776d53a8bd5f9bbc1bf23ab5754a523bf8900
ObedOmwega <139385466+ObedOmwega@users.noreply.github.com>
2024-03-10 17:48:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,59 @@
 document.addEventListener('DOMContentLoaded', function() {
 
   const signUpBtn = document.querySelector('.signup-btn');
-
   const loginBtn = document.querySelector('.login-btn');
-
-  signUpBtn.addEventListener('mouseover', function() {
-
-    signUpBtn.style.color = getRandomColor();
-
-  });
-
-  signUpBtn.addEventListener('mouseout', function() {
-
-    signUpBtn.style.color = 'white';
-
-  });
-
-  loginBtn.addEventListener('mouseover', function() {
-
-    loginBtn.style.color = getRandomColor();
-
-  });
-
-  loginBtn.addEventListener('mouseout', function() {
-
-    loginBtn.style.color = 'white';
-
-  });
+  const homeBtn = document.querySelector('.home-btn');
+  const discoverBtn = document.querySelector('.discover-btn');
+  const studyGroupsBtn = document.querySelector('.study-groups-btn');
+  const profileBtn = document.querySelector('.profile-btn');
 
   signUpBtn.addEventListener('click', function() {
-
-    alert('Sign Up button clicked!');
-
+    window.location.href = 'signup.html';
   });
 
   loginBtn.addEventListener('click', function() {
+    window.location.href = 'login.html';
+  });
 
-    alert('Log In button clicked!');
+  homeBtn.addEventListener('click', function() {
+    alert('Sign Up or Log In First!');
+  });
 
+  discoverBtn.addEventListener('click', function() {
+    alert('Sign Up or Log In First!');
+  });
+
+  studyGroupsBtn.addEventListener('click', function() {
+    alert('Sign Up or Log In First!');
+  });
+
+  profileBtn.addEventListener('click', function() {
+    alert('Sign Up or Log In First!');
+  });
+
+  signUpBtn.addEventListener('mouseover', function() {
+    signUpBtn.style.color = getRandomColor();
+  });
+
+  signUpBtn.addEventListener('mouseout', function() {
+    signUpBtn.style.color = 'white';
+  });
+
+  loginBtn.addEventListener('mouseover', function() {
+    loginBtn.style.color = getRandomColor();
+  });
+
+  loginBtn.addEventListener('mouseout', function() {
+    loginBtn.style.color = 'white';
   });
 
   function getRandomColor() {
-
     const letters = '0123456789ABCDEF';
-
     let color = '#';
-
     for (let i = 0; i < 6; i++) {
-
       color += letters[Math.floor(Math.random() * 16)];
-
     }
-
     return color;
-
   }
 
 });
